59e8d1c2ee270455e415f02aeff7756d50d55b56
YiShao0829 <t107360106@ntut.org.tw>
2020-12-24 23:54:08 +0800

    SQLite

---
+++ app/src/main/java/com/example/kotlin_hw_sqlite/MyDBHelper.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_hw_sqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyDBHelper (context: Context): SQLiteOpenHelper(context, name, null, version) {
+    companion object {
+        private const val name = "mdatabase.db"
+        private const val version = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE myTable(book text PRIMARY KEY, price integer NOT NULL)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS myTable")
+        onCreate(db)
+    }
+}
